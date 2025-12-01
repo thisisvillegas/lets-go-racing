2bb5ddb59bb0b89c9cac60e6be97d7af723beba2
Andres Villegas <thisisvillegas@gmail.com>
2025-11-30 21:19:16 -0600

    sentry integration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,18 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { authHttpInterceptorFn } from '@auth0/auth0-angular';
+import * as Sentry from '@sentry/angular';
 
 import { routes } from './app.routes';
 import { authConfig } from './auth.config';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([authHttpInterceptorFn])
     ),
-    provideAuth0(authConfig)
+    provideAuth0(authConfig),
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: false, // Don't show the Sentry dialog to users
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [],
+    },
   ]
 };
